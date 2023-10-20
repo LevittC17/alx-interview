09e7f897c2e0a257152c55b2e2d0ab549b23dc20
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-20 09:16:48 +0000

    include error checking and handling unexpected input formats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,8 +25,7 @@ def main():
     Main function for processing log lines and computing statistics.
     """
     total_size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
-                    404: 0, 405: 0, 500: 0}
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     line_count = 0
 
     try:
@@ -35,17 +34,22 @@ def main():
             if len(parts) < 9:
                 continue
 
-            status_code = int(parts[-2])
-            file_size = int(parts[-1])
+            try:
+                status_code = int(parts[-2])
+                file_size = int(parts[-1])
 
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-                total_size += file_size
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                    total_size += file_size
 
-            line_count += 1
+                line_count += 1
 
-            if line_count % 10 == 0:
-                print_stats(total_size, status_codes)
+                if line_count % 10 == 0:
+                    print_stats(total_size, status_codes)
+
+            except ValueError:
+                # Handle lines with invalid status code gracefully
+                continue
 
     except KeyboardInterrupt:
         pass
