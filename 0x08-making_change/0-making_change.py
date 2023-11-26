3e89458104b2d0e200f7d8305cd581ba8ef43152
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-26 11:28:18 +0000

    PEP8

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,7 +15,8 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Handle base case: If there are no coins, or total is not possible with given coins
+    # Handle base case: If there are no coins, or total is not possible with
+    # given coins
     if not coins or min(coins) > total:
         return -1
 
@@ -26,7 +27,8 @@ def makeChange(coins, total):
     # Iterate through coins and amounts, updating the table
     for coin in coins:
         for amount in range(coin, total + 1):
-            dp_table[amount] = min(dp_table[amount], dp_table[amount - coin] + 1)
+            dp_table[amount] = min(
+                dp_table[amount], dp_table[amount - coin] + 1)
 
     # Return the result for the original total
     return dp_table[total] if dp_table[total] != float('inf') else -1
