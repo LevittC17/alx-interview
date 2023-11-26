046c780dd52790e2fcd0366ba54cac2c5325653b
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-26 11:27:36 +0000

    Runtime Efficient

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,25 +12,21 @@ def makeChange(coins, total):
     given amount total
     """
     # Check if the total is 0 or less
-    if (total <= 0):
+    if total <= 0:
         return 0
 
-    # Handle the base cases: If there are no coins, or total is not possible
-    # with given coins
+    # Handle base case: If there are no coins, or total is not possible with given coins
     if not coins or min(coins) > total:
         return -1
 
-    # Initialize a table to score results
-    # (Hint: This can be a list where the index represents the total amount)
+    # Initialize a table to store results
     dp_table = [float('inf')] * (total + 1)
     dp_table[0] = 0  # Base case: 0 coins needed for total of 0
 
-    # Populate the table based on the base cases and state transition
+    # Iterate through coins and amounts, updating the table
     for coin in coins:
         for amount in range(coin, total + 1):
-            dp_table[amount] = min(
-                dp_table[amount], dp_table[amount - coin] + 1)
+            dp_table[amount] = min(dp_table[amount], dp_table[amount - coin] + 1)
 
-    # Return the result for the orginal total
-    # If it's not possible to make the change, return -1
+    # Return the result for the original total
     return dp_table[total] if dp_table[total] != float('inf') else -1
