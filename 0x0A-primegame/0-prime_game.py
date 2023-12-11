45f5cba28a698ca254bcf05d44dc963b65ea2db6
LevittC17 <LeewednesdayC17@protonmail.com>
2023-12-10 18:09:13 -0800

    code optimization

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,90 +7,49 @@ Primer Game
 
 def isWinner(x, nums):
     """
-    Determines the winner of each round of the prime game.
+    Determines the winner of the prime game.
 
     Args:
     - x (int): Number of rounds.
-    - nums (list): Array of integers representing the end range for each round.
+    - nums (list): List of integers representing the end range for each round.
 
     Returns:
     - str or None: Name of the player that won the most rounds.
                    Returns None if the winner cannot be determined.
 
-    Constraints:
-    - x and n will not be larger than 10000.
-    - Cannot import any packages.
-
     Example:
-    >>> isWinner(3, [4, 5, 1])
+    >>> find_winner(3, [4, 5, 1])
     'Ben'
     """
-    def is_prime(num):
-        """
-        Checks if a number is prime.
-
-        Args:
-        - num (int): The number to check.
-
-        Returns:
-        - bool: True if the number is prime, False otherwise.
-        """
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def get_primes(n):
-        """
-        Generates a list of prime numbers up to n.
-
-        Args:
-        - n (int): The end range.
-
-        Returns:
-        - list: List of prime numbers.
-        """
-        primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
-        return primes
-
-    def remove_multiples(remaining, prime):
-        """
-        Removes multiples of a prime number from the remaining numbers.
-
-        Args:
-        - remaining (list): List of remaining numbers.
-        - prime (int): Prime number to remove multiples of.
-
-        Returns:
-        - list: Updated list of remaining numbers.
-        """
-        return [num for num in remaining if num % prime != 0]
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        primes = get_primes(n)
-        remaining = list(range(1, n + 1))
-
-        maria_turn = True
-        while primes and any(prime <= max(remaining) for prime in primes):
-            prime = primes.pop(0)
-            if maria_turn:
-                ben_wins += 1
-            else:
-                maria_wins += 1
-            remaining = remove_multiples(remaining, prime)
-            maria_turn = not maria_turn
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    if not nums or x < 1:
         return None
+
+    max_num = max(nums)
+
+    is_prime = [True for _ in range(max(max_num + 1, 2))]
+
+    # Sieve of Eratosthenes to find prime numbers
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not is_prime[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            is_prime[j] = False
+
+    is_prime[0] = is_prime[1] = False
+
+    prime_count = 0
+    for i in range(len(is_prime)):
+        if is_prime[i]:
+            prime_count += 1
+        is_prime[i] = prime_count
+
+    # Count the number of rounds player 1 wins
+    player1_wins = sum(is_prime[x] % 2 == 1 for x in nums)
+
+    # Determine the winner based on the number of rounds won
+    if player1_wins * 2 == len(nums):
+        return None
+    elif player1_wins * 2 > len(nums):
+        return "Maria"
+    else:
+        return "Ben"
